cdcbb7e20e0490fff1fb1754645215bbb09aefd5
alemasle <alexislm35@outlook.fr>
2018-07-16 16:24:08 +0200

    Formattage

--- csv_kernels/DBCredentials.py
+++ csv_kernels/DBCredentials.py
@@ -1,10 +1,10 @@
 db = [{
-    "creds":{
-        "user":"csvgen",
-        "passwd":"yHGE0Km4bxxKoYdb",
-        "host":"148.60.11.195",
+    "creds": {
+        "user": "csvgen",
+        "passwd": "yHGE0Km4bxxKoYdb",
+        "host": "148.60.11.195",
         "db": "IrmaDB_prod",
     },
-    "bz2":True,
-    "table":"Compilations"
+    "bz2": True,
+    "table": "Compilations"
 }]
